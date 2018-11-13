a7c7b613e3866ef9161f85259ba3fea23f5bb972
A403163 <erich.brandl@gmail.com>
2018-11-13 19:49:16 +0100

    extract images from mnist database

--- public_html/js/util/mnist.js
+++ public_html/js/util/mnist.js
@@ -1,22 +1,51 @@
 const fs = require('fs');
 const _ = require('underscore');
+const PNGImage = require('pngjs-image');
+
+const DIM = 28;
+const DIMSQR = DIM * DIM;
+
+
 
 const readMnistDB = function () {
 
-   const lbl = fs.readFileSync('public_html/data/mnist/t10k-labels.idx1-ubyte').slice(8)
-   const img = fs.readFileSync('public_html/data/mnist/t10k-images.idx3-ubyte').slice(16)
+   const lbl = fs.readFileSync('public_html/data/mnist/t10k-labels.idx1-ubyte').slice(8);
+   const imgs = fs.readFileSync('public_html/data/mnist/t10k-images.idx3-ubyte').slice(16);
 
-   const db = {};
-   _.range(10).forEach(n => db[n] = []);
-   
-   for (let i = 0; i < lbl.length; i++) {
-      const DIM = 28*28;
-      const buf = [];
-      for( const v of img.slice(i*DIM, (i+1)*DIM).values() ) buf.push(v);
-      db[lbl[i]].push(buf);
+   const creImg = (arr) => {
+      var img = PNGImage.createImage(DIM, DIM);
+      for (let i = 0; i < arr.length; i++) {
+         const pix = 255 - arr[i]
+         const [r, c] = [i % DIM, Math.floor(i / DIM)];
+         img.setAt(r, c, {red: pix, green: pix, blue: pix, alpha: 255});
+      }
+      return img;
+   };
+
+   function f(imgarr, n) {
+      if (n >= imgarr.length)
+         return;
+
+      const path = 'public_html/data/mnist/imgs/img-' + lbl[n] + '-' + n + '.png'
+      console.log('PATH', path)
+
+      imgarr[n].writeImage('public_html/data/mnist/imgs/img-' + lbl[n] + '-' + n + '.png', (err) => {
+         err && console.log('Not written to the file' + err);
+         console.log('AAAAAA', n)
+         f(imgarr, n + 1);
+      });
    }
-   
-   return db;
+
+   const imgarr = [];
+   for (let x = 0; x < lbl.length; x++) {
+      imgarr.push(creImg(imgs.slice(x * DIMSQR, (x + 1) * DIMSQR)));
+   }
+   console.log(imgarr.length, imgarr[0]);
+
+   f(imgarr, 0);
+
 }
 
-console.log(JSON.stringify(readMnistDB()[0][0]))
+//readMnistDB(0,5000);
+readMnistDB();
+
