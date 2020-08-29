54e8f147ba2a64ab8df97471dd6dfb1e75a9dd81
A403163 <erich.brandl@gmail.com>
2020-08-29 12:53:08 +0200

    fix and improve: jest tests

--- tests/ocr.test.js
+++ tests/ocr.test.js
@@ -1,19 +1,32 @@
 const range = n => [...Array(n).keys()];
-const dim = '6x4';
-const dbtrain = require('../data/dbjs/ebdb-train-' + dim);
-const dbtest = require('../data/dbjs/ebdb-test-' + dim);
-const ocr = require('../src/ocr/ocr')(dbtrain);
+const ocr = require('../src/ocr/ocr')
 
-const counter = { ok: 0, nok: 0, all: 0 };
-const run = n => dbtest[n].forEach(checkDigit => {
-  const guessOk = ocr.findNearestDigit(checkDigit.imgvec)[0].digit === n;
-  counter.all++;
-  counter.ok += guessOk;
-  counter.nok += !guessOk;
+const run = (dim, dbname) => {
+  const counter = { ok: 0, nok: 0, all: 0 };
+  const dbtrain = require(`../data/dbjs/${dbname}-train-${dim}`);
+  const dbtest = require(`../data/dbjs/${dbname}-test-${dim}`);
+  const ocrx = ocr(dbtrain);
+  range(10).forEach(n => {
+    dbtest[n].forEach(checkDigit => {
+      const guessOk = ocrx.findNearestDigit(checkDigit.imgvec)[0].digit === n;
+      counter.all++;
+      counter.ok += guessOk;
+      counter.nok += !guessOk;
+    })
+  })
+  return counter;
+};
+
+test('ocr 6x4', () => {
+  const counter = run('6x4', 'ebdb-mnist');
+  const quot = counter.ok / counter.all;
+  console.log(counter, quot);
+  expect(quot).toBeGreaterThan(0.975);
 });
 
-test('first ocr', () => {
-  range(10).forEach(n => run(n));
+xtest('ocr 8x6', () => {
+  const counter = run('8x6', 'ebdb');
   const quot = counter.ok / counter.all;
+  console.log(counter, quot);
   expect(quot).toBeGreaterThan(0.975);
 });
